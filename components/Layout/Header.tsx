90149a52b8c4c5905b7bb23ac6aeeae6cd453c92
yusixian <3450414733@qq.com>
2023-03-15 22:06:15 +0800

    feat: header icon and back

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/router';
 import { useToggleTheme } from '../../hooks/useToggleTheme';
 import Icon from '../Icon';
 import { IconType } from '../Icon/type';
 
 export function Header() {
   const toggleTheme = useToggleTheme();
-
+  const router = useRouter();
   return (
-    <header className="flex h-12 w-full items-center justify-end gap-2 px-2 ">
+    <header className="flex h-16 w-full items-center justify-between gap-2 p-2">
+      <div className="flex-grow cursor-pointer" onClick={() => router.push('/')}>
+        <img src="/favicon.ico" alt="logo" className="aspect-square h-12" />
+      </div>
       <Icon className="text-4xl" type={IconType.LIGHT_OR_DARK} onClick={toggleTheme} />
     </header>
   );
